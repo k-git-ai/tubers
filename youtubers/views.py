80713a50d35d7df4d44823d652214d32df86442d
k-git-ai <koushikhait549@gmail.com>
2021-01-20 15:24:56 +0530

    second commit

--- youtubers/views.py
+++ youtubers/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Youtuber
 
 
 # Create your views here.
 def youtubers(request):
-    return HttpResponse('youtubers', render(request, 'youtubers/youtuber.html'))
+    ytrs = Youtuber.objects.order_by('-created_date')
+    data = {
+        'ytrs': ytrs
+    }
+    return render(request, 'youtubers/youtuber.html', data)
 
 
 def serach(request):
     return HttpResponse('search', render(request, 'youtubers/serach.html'))
 
 
-def youtubers_detail(request):
-    return HttpResponse('youtubers_detail', render(request, 'youtubers/youtubers_detail.html'))
+def youtubers_detail(request, id):
+    return render(request, 'youtubers/youtubers_detail.html')
